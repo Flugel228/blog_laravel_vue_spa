a818ad10ada42c80da9b2c557278598e0683052c
Flugel228 <imflugel@gmail.com>
2023-05-28 13:09:51 +0300

    minor fixes in the admin

--- resources/views/admin/post/create.blade.php
+++ resources/views/admin/post/create.blade.php
@@ -27,22 +27,6 @@
                 </div>
             </div><!-- /.container-fluid -->
         </div>
-        <!-- /.content-header -->
-        <section class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2 ml-1">
-                    <div class="col-sm-6">
-                        <h1>Посты</h1>
-                    </div>
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Главная</a></li>
-                            <li class="breadcrumb-item active">Посты</li>
-                        </ol>
-                    </div>
-                </div>
-            </div><!-- /.container-fluid -->
-        </section>
         <!-- Main content -->
         <section class="content">
             <div class="container-fluid">
--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -8,7 +8,7 @@
             <div class="container-fluid">
                 <div class="row mb-2 ml-1">
                     <div class="col-sm-6">
-                        <h1>Теги</h1>
+                        <h1>Пользователи</h1>
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
